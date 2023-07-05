6a0f2370f7f70edb1aedd2dfd5483330728f33ef
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2023-07-05 18:55:10 -0500

    js elements loaded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './style.css'
+
+const mainContainer = document.getElementById("mainContainer")
+
+function addHTMLToMainContainer() {
+const htmlStructure = `
+    <div id="navBar">
+      <div id="buttonDiv">
+        <button id="buttons" class="str">STR</button>
+        <button id="buttons" class="agi">AGI</button>
+        <button id="buttons" class="int">INT</button>
+        <button id="buttons" class="uni">UNI</button>
+      </div>
+    </div>
+    <div id="homeMessage">These are the top 6 heroes sorted by attribute for Dota 2</div>
+    <div id="mainImages">
+    </div>
+    <div id="footer">Contact Me</div>
+  `;
+
+    mainContainer.innerHTML = htmlStructure
+}
+
+addHTMLToMainContainer()
